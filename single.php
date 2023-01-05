f9b65355158b2a70aaa5f767b03ca904cbfaa7ae
Casey Milne <casey@saberwp.com>
2023-01-04 23:01:54 -0600

    Added blog single comps.

--- single.php
+++ single.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
 <?php require_once(get_template_directory() . '/components/marketing/page-sections/content-sections/centered.php'); ?>
+<?php require_once(get_template_directory() . '/components/marketing/page-sections/team-sections/dark-version-with-large-images.php'); ?>
 <?php get_footer(); ?>
