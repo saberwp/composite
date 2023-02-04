a5bb328d2f9eb58af3bc191470b0abf1cf543932
Casey Milne <casey@saberwp.com>
2023-02-03 21:25:12 -0600

    Footer manifest with data, data load and set for footer.

--- components/footers/4-column-with-company-mission/component.php
+++ components/footers/4-column-with-company-mission/component.php
@@ -1,17 +1,12 @@
-<?php
-
-$data = new stdClass;
-$data->year = date('Y');
-
-?>
-
 <footer class="bg-white" aria-labelledby="footer-heading">
   <h2 id="footer-heading" class="sr-only">Footer</h2>
   <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
     <div class="xl:grid xl:grid-cols-3 xl:gap-8">
       <div class="space-y-8">
         <img class="h-7" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Company name">
-        <p class="text-sm leading-6 text-gray-600">Making the world a better place through constructing elegant hierarchies.</p>
+        <p class="text-sm leading-6 text-gray-600">
+					<?php echo $data->company_mission; ?>
+				</p>
         <div class="flex space-x-6">
           <a href="#" class="text-gray-400 hover:text-gray-500">
             <span class="sr-only">Facebook</span>
@@ -113,7 +108,7 @@ $data->year = date('Y');
       </div>
     </div>
     <div class="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
-      <p class="text-xs leading-5 text-gray-500">&copy; <?php echo $data->year; ?> Your Company, Inc. All rights reserved.</p>
+      <p class="text-xs leading-5 text-gray-500">&copy; <?php echo $data->year; ?> <?php echo $data->company_name; ?>. All rights reserved.</p>
     </div>
   </div>
 </footer>
--- footer.php
+++ footer.php
@@ -1,9 +1,32 @@
 <?php
 
+// Set default footer if none is set in option "footer_default".
 $footer_default = get_field( 'footer_default', 'option' );
 if ( ! $footer_default ) {
 	$footer_default = 'default';
 }
+
+// Load component manifest and set data for the component.
+$manifest_path = get_template_directory() . '/components/footers/' . $footer_default . '/manifest.json';
+ob_start();
+require $manifest_path;
+$manifestJson = ob_get_contents();
+ob_end_clean();
+$manifest = json_decode( $manifestJson );
+$data = $manifest->data;
+
+// Company name.
+$company_name = get_field('company_name', 'option');
+if( $company_name ) {
+	$data->company_name = $company_name;
+}
+
+// Company mission statement.
+$company_mission = get_field('company_mission', 'option');
+if( $company_mission ) {
+	$data->company_mission = $company_mission;
+}
+
 $component_path = get_template_directory() . '/components/footers/' . $footer_default . '/component.php';
 require $component_path;
 
