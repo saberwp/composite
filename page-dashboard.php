b177df6490d05dadecfee2803b47051acd94cccc
Casey Milne <casey@saberwp.com>
2023-02-26 01:04:10 -0600

    Constrain shop main, remove menu output.

--- page-dashboard.php
+++ page-dashboard.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-DASH
+<div class="bg-gray-400 p-4 my-12">
+	<h1 class="font-bold text-2xl">Dashboard</h1>
+	<div class="flex gap-8">
+		<div class="basis-4/12 bg-gray-100 rounded-md">
+			MENU
+		</div>
+		<div class="basis-8/12">
+			BODY
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
