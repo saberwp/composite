bc421180458a9099de6cd3856bb7a16f0410e6af
Casey Milne <casey@saberwp.com>
2023-03-01 16:22:53 -0600

    Improve archive default template.

--- archive.php
+++ archive.php
@@ -1,11 +1,15 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : ?>
-  <?php while ( have_posts() ) : the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_excerpt(); ?>
-  <?php endwhile; ?>
-  <?php the_posts_pagination(); ?>
-<?php else : ?>
-  <p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'composite' ); ?></p>
-<?php endif; ?>
+
+<div class="p-4 my-8 md:max-w-2xl md:mx-auto md:px-0">
+`	<?php if ( have_posts() ) : ?>
+	  <?php while ( have_posts() ) : the_post(); ?>
+	    <h2 class="font-bold text-2xl my-4"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	    <?php the_excerpt(); ?>
+	  <?php endwhile; ?>
+	  <?php the_posts_pagination(); ?>
+	<?php else : ?>
+	  <p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'composite' ); ?></p>
+	<?php endif; ?>
+</div>
+
 <?php get_footer(); ?>
